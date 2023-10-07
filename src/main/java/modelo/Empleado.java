91e43fd0480ef7ecdff99a5cf2d31b1cdfd18f51
AxelGutierrezV <99294133+AxelGutierrezV@users.noreply.github.com>
2023-10-07 17:24:54 -0500

    Add files via upload

--- src/main/java/modelo/Empleado.java
+++ src/main/java/modelo/Empleado.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -84,6 +84,11 @@ public class Empleado {
                 e = "Inactivo";
             }
         return e;
+    }
+    
+    @Override
+    public String toString(){
+        return CodEmpleado + " - " + nombre + " - " + apellido + " - " + pass + " - " + codPerfil + " - " + perfil + " - " + estado;
     }
     
 }
